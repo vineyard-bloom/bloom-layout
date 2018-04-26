ded6f01e7e997148b0005d59888970aa70c50f15
Katie Foster <fosterkatiea@gmail.com>
2018-04-26 15:59:29 -0600

    updating export to include ProgressBar

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import Accordion from './components/accordion'
 import Alert from './components/alert'
 import BannerAlert from './components/banner-alert'
 import Modal from './components/modal'
+import ProgressBar from './components/progress-bar'
 import Rating from './components/rating'
 import Table from './components/table'
 import Tooltip from './components/tooltip'
 
-export { Accordion, Alert, BannerAlert, Modal, Rating, Table, Tooltip }
+export {
+  Accordion,
+  Alert,
+  BannerAlert,
+  Modal,
+  ProgressBar,
+  Rating,
+  Table,
+  Tooltip
+}
